00852f6414f8a4b8b26c5d6f1d5c2053d43c2b33
RafaBessa <rafablou@hotmail.com>
2020-09-25 18:42:02 -0300

    Projeto com o Joy e turtle configurado

---
+++ catkin_ws/src/beginner_tutorials/scripts/joymove.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Joy
+
+
+def callback(data):
+    twist = Twist()
+    twist.linear.x = 4*data.axes[1]
+    twist.angular.z = 4*data.axes[0]
+    pub.publish(twist)
+
+def publisher():
+    global pub
+    pub = rospy.Publisher('/turtle1/cmd_vel', Twist)
+    #subscribe no joy pra pegar os inputs
+    rospy.Subscriber("joy", Joy, callback)
+
+    rospy.init_node('Joy2Turtle')
+    rospy.spin()
+
+
+def move():
+    # Starts a new node
+    rospy.init_node('robot_cleaner', anonymous=True)
+    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    vel_msg = Twist()
+
+    #Receiveing the user's input
+    print("Let's move your robot")
+    speed = input("Input your speed:")
+    distance = input("Type your distance:")
+    isForward = input("Foward?: ")#True or False
+
+    #Checking if the movement is forward or backwards
+    if(isForward):
+        vel_msg.linear.x = abs(speed)
+    else:
+        vel_msg.linear.x = -abs(speed)
+    #Since we are moving just in x-axis
+    vel_msg.linear.y = 0
+    vel_msg.linear.z = 0
+    vel_msg.angular.x = 0
+    vel_msg.angular.y = 0
+    vel_msg.angular.z = 0
+
+    while not rospy.is_shutdown():
+
+        #Setting the current time for distance calculus
+        t0 = rospy.Time.now().to_sec()
+        current_distance = 0
+
+        #Loop to move the turtle in an specified distance
+        while(current_distance < distance):
+            #Publish the velocity
+            velocity_publisher.publish(vel_msg)
+            #Takes actual time to velocity calculus
+            t1=rospy.Time.now().to_sec()
+            #Calculates distancePoseStamped
+            current_distance= speed*(t1-t0)
+        #After the loop, stops the robot
+        vel_msg.linear.x = 0
+        #Force the robot to stop
+        velocity_publisher.publish(vel_msg)
+
+if __name__ == '__main__':
+    publisher()
+#    try:
+#         #Testing our function
+#        move()
+#     except rospy.ROSInterruptException: pass
